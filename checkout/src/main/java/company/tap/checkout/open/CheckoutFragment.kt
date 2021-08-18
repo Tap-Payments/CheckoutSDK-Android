7f4baba69308fdec716581eeadd398decb0f4a3e
olamonir <ola.monir7@gmail.com>
2021-08-18 11:36:21 +0300

    add close button to bottom sheet

--- checkout/src/main/java/company/tap/checkout/open/CheckoutFragment.kt
+++ checkout/src/main/java/company/tap/checkout/open/CheckoutFragment.kt
@@ -73,7 +73,9 @@ class CheckoutFragment : TapBottomSheetDialog(), TapBottomDialogInterface, Inlin
         val cardViewModel: CardViewModel by viewModels()
         this.viewModel = viewModel
         _Context?.let { cardViewModel.getContext(it) }
-        closeIcon?.setOnClickListener { bottomSheetDialog.dismiss() }
+        closeIcon?.setOnClickListener {
+            bottomSheetDialog.hide()
+             }
 
         val view = inflater.inflate(R.layout.fragment_checkouttaps, container, false)
         backgroundColor = (Color.parseColor(ThemeManager.getValue("GlobalValues.Colors.clear")))
