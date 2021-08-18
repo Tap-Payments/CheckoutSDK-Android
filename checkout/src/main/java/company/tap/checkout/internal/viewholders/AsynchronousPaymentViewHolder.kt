43fa7b0f64e8bc63a429d7e8864bbb76e64eeb2d
AhlaamK-tap <a.kazi@tap.company>
2021-08-18 12:25:16 +0300

    AsynchronousPaymentViewHolder

--- checkout/src/main/java/company/tap/checkout/internal/viewholders/AsynchronousPaymentViewHolder.kt
+++ checkout/src/main/java/company/tap/checkout/internal/viewholders/AsynchronousPaymentViewHolder.kt
@@ -1,10 +1,40 @@
 package company.tap.checkout.internal.viewholders
 
+import android.content.Context
+
+import android.view.LayoutInflater
+import android.view.View
+import company.tap.checkout.R
+import company.tap.checkout.internal.enums.SectionType
+
+
 /**
  * Created by AhlaamK on 8/18/21.
 
 Copyright (c) 2021    Tap Payments.
 All rights reserved.
  **/
-class AsynchronousPaymentViewHolder {
+class AsynchronousPaymentViewHolder (private val context: Context) : TapBaseViewHolder {
+
+
+    override val view: View =
+        LayoutInflater.from(context).inflate(R.layout.asynchronous_layout, null)
+
+    override val type = SectionType.BUSINESS
+
+    private var merchantName: String? = null
+    private var merchantLogo: String? = null
+
+
+    init {
+        bindViewComponents()
+    }
+
+    override fun bindViewComponents() {
+
+
+    }
+
+
+
 }
