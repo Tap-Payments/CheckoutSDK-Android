cfa0d1fa0aeb0156bec7a9ed539e71ef1f2dca7c
AhlaamK-tap <a.kazi@tap.company>
2023-05-30 11:14:58 +0300

    re
    activateActionButtonForGPay

--- checkout/src/main/java/company/tap/checkout/internal/viewmodels/CheckoutViewModel.kt
+++ checkout/src/main/java/company/tap/checkout/internal/viewmodels/CheckoutViewModel.kt
@@ -2092,7 +2092,7 @@ open class CheckoutViewModel : ViewModel(), BaseLayoutManager, OnCardSelectedAct
                     } else if ((savedCardsModel as PaymentOption).paymentType == PaymentType.GOOGLE_PAY) {
                         //  removeViews(amountViewHolder, cardViewHolder, paymentInlineViewHolder)
                         // checkoutFragment.checkOutActivity?.handleGooglePayApiCall(savedCardsModel as PaymentOption)
-                        //activateActionButtonForGPay()
+
                         activateActionButton((savedCardsModel as PaymentOption))
                         setPayButtonAction(PaymentType.GOOGLE_PAY, savedCardsModel)
                         PaymentDataSource.setWebViewType(WebViewType.REDIRECT)
@@ -2110,25 +2110,7 @@ open class CheckoutViewModel : ViewModel(), BaseLayoutManager, OnCardSelectedAct
         }
     }
 
-    private fun activateActionButtonForGPay() {
-        val payString: String = LocalizationManager.getValue("pay", "ActionButton")
-        saveCardSwitchHolder?.view?.cardSwitch?.payButton?.isActivated
-        saveCardSwitchHolder?.view?.cardSwitch?.payButton?.changeButtonState(ActionButtonState.LOADING)
-        saveCardSwitchHolder?.view?.cardSwitch?.payButton?.setButtonDataSource(
-            true,
-            "en",
-            "",
-            Color.parseColor(ThemeManager.getValue("actionButton.Valid.paymentBackgroundColor")),
-            Color.parseColor(ThemeManager.getValue("actionButton.Valid.titleLabelColor")),
-        )
-
-        saveCardSwitchHolder?.view?.cardSwitch?.showOnlyPayButton()
-
-        saveCardSwitchHolder?.view?.cardSwitch?.payButton?.isActivated
-        //saveCardSwitchHolder?.view?.cardSwitch?.payButton?.changeButtonState(ActionButtonState.LOADING)
-
-
-    }
+  
 
 
     private fun activateActionButton(
