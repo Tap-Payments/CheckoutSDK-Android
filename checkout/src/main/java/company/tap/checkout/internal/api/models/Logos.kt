44b4b2a6b75847ee58fcdf0c2092a2577af4f9e7
AhlaamK-tap <a.kazi@tap.company>
2023-06-01 16:29:59 +0300

    update model

--- checkout/src/main/java/company/tap/checkout/internal/api/models/Logos.kt
+++ checkout/src/main/java/company/tap/checkout/internal/api/models/Logos.kt
@@ -19,12 +19,13 @@ data class Logos(@SerializedName("dark") @Expose
 
 ) : Serializable
 
-data class Dark( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String, @SerializedName("currency_widget") @Expose  var currencyWidget: CurrencyWidgetModel,  @SerializedName("disabled") @Expose  var disabled: DisabledModel ):Serializable
-data class DarkColored( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String,  @SerializedName("currency_widget") @Expose  var currencyWidget: CurrencyWidgetModel,  @SerializedName("disabled") @Expose  var disabled: DisabledModel ):Serializable
+data class Dark( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String, @SerializedName("currency_widget") @Expose  var currencyWidget: CurrencyWidgetModel,  @SerializedName("disabled") @Expose  var disabled: DisabledModel, @SerializedName("card_icon") @Expose  var cardIcon: CardIconModel ):Serializable
+data class DarkColored( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String,  @SerializedName("currency_widget") @Expose  var currencyWidget: CurrencyWidgetModel,  @SerializedName("disabled") @Expose  var disabled: DisabledModel, @SerializedName("card_icon") @Expose  var cardIcon: CardIconModel ):Serializable
 
-data class Light( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String,  @SerializedName("currency_widget") @Expose  var currencyWidget: CurrencyWidgetModel,  @SerializedName("disabled") @Expose  var disabled: DisabledModel ):Serializable
-data class LightMono( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String,  @SerializedName("currency_widget") @Expose  var currencyWidget: CurrencyWidgetModel,  @SerializedName("disabled") @Expose  var disabled: DisabledModel ):Serializable
+data class Light( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String,  @SerializedName("currency_widget") @Expose  var currencyWidget: CurrencyWidgetModel,  @SerializedName("disabled") @Expose  var disabled: DisabledModel, @SerializedName("card_icon") @Expose  var cardIcon: CardIconModel ):Serializable
+data class LightMono( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String,  @SerializedName("currency_widget") @Expose  var currencyWidget: CurrencyWidgetModel,  @SerializedName("disabled") @Expose  var disabled: DisabledModel ,  @SerializedName("card_icon") @Expose  var cardIcon: CardIconModel):Serializable
 
 
 data class DisabledModel( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String ):Serializable
 data class CurrencyWidgetModel( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String ):Serializable
+data class CardIconModel( @SerializedName("svg") @Expose  var svg: String ,@SerializedName("png") @Expose  var png: String ):Serializable
